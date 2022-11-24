27f07ce0c3bd492839c564f09c1b000fe96fed1f
Rui Cavaco <rui.cavaco@codex-soft.com>
2022-11-24 16:04:57 -0100

    fix to config file

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': '//photo-editor-heroku.herokuapp.com/photoEditor',
+    'mongoUrl': 'mongodb://photo-editor-heroku.herokuapp.com/photoEditor',
     'user': 'ruiahau@hotmail.com',
     'pass': 'AhauR580'
 }
