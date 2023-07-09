8e45abe3c609b34a0804836a5c352094eee3333f
Cavacoz <108743541+Cavacoz@users.noreply.github.com>
2023-07-09 17:32:07 +0000

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb+srv://Cavaco:SamsungR580@photoeditor.q8hcsyf.mongodb.net/?retryWrites=true&w=majority',
-    'user': 'ruiahau@hotmail.com',
-    'pass': 'AhauR580'
+    'mongoUrl': 'mongodb+srv://'username':'password'@photoeditor.q8hcsyf.mongodb.net/?retryWrites=true&w=majority',
+    'user': 'email@email.com',
+    'pass': 'password'
 }
